3eda029558712aad5d5e0cd2a73de1a65cf6d856
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-10-02 12:59:22 +0200

    more work on the refactoring
    build probably broken here

--- flippers.js
+++ flippers.js
@@ -419,12 +419,31 @@ var GameState = (function() {
 	    }
 	}
 
+	function elementAtCell( cell ) {
+	    return elementAt( cell.col, cell.row );
+	}
+
+	function removeElementAtCell( cell ) {
+	    var el = elementAtCell( cell );
+	    if( el ) {
+		arrayRemoveElement( state.elements, el );
+	    }
+	}
+
+	function setElement( element ) {
+	    removeElementAtCell( element.col, element.row );
+	    state.elements.push( element );
+	}
+
 	return {
 	    start: start,
 	    advance: advance,
 	    status: status,
 	    save: save,
 	    render: render,
+	    elementAtCell: elementAtCell,
+	    removeElementAtCell: removeElementAtCell,
+	    setElement: setElement,
 	    elements: function(){ return state.elements; },
 	    ball: function(){ return state.ball; },
 	    size: function(){ return state.size; }
@@ -438,7 +457,7 @@ var GameState = (function() {
     }
 })();
 
-var SmoothGameState = function( gamestate ) {
+var SmoothGameState = { wrap: function( gamestate ) {
     var target = 40;
     var counter = 0;
     var timerId = null;
@@ -525,25 +544,7 @@ var SmoothGameState = function( gamestate ) {
 	running: running,
 	render: render
     }
-};
-
-var canvas = null;
-var ctx = null;
-var jqcanvas = null;
-var kb = null;
-var myState = null;
-
-var gamegraphics = null;
-
-var cellsize = 60.0;
-var boardColumns = 5, boardRows = 5;
-var buildMode = true;
-var canvasWidth = 480;
-var canvasHeight = 480;
-
-var gamestate = null;
-
-var buildModeSerialization = null;
+} };
 
 var predefinedLevels = {
     "Puzzle 2 -- place a single flipper":
@@ -575,185 +576,6 @@ function colourOf( base, deactivated ) {
     return "#f55";
 }
 
-function startRun() {
-    if( !buildMode ) {
-	return;
-    }
-	
-    $("#startstopbutton").html( "Stop" );
-
-    buildMode = false;
-    gamestate = initialGameState();
-
-    buildModeSerialization = serializeGame();
-
-    $("#loadbutton").prop( "disabled", true );
-    $("#savebutton").prop( "disabled", true );
-}
-
-function stopRun() {
-    if( buildMode ) {
-	return;
-    }
-
-    $("#startstopbutton").html( "Start" );
-
-    buildMode = true;
-    gamestate = null;
-
-    if( buildModeSerialization ) {
-	unserializeGame( buildModeSerialization );
-    }
-
-    $("#loadbutton").prop( "disabled", false );
-    $("#savebutton").prop( "disabled", false );
-}
-
-function saveLevel() {
-    $("#leveldata").val( serializeGame() );
-}
-
-function loadLevel() {
-    unserializeGame( $("#leveldata").val() );
-}
-
-function serializeGame() {
-    return JSON.stringify(
-	{rows: boardRows,
-	 cols: boardColumns,
-	 contents: elements}
-    );
-}
-
-function unserializeGame( data ) {
-    var x = JSON.parse( data );
-    boardColumns = x.cols;
-    boardRows = x.rows;
-    elements = x.contents;
-
-    autofitBoard();
-}
-
-function initialGameState() {
-    return {col: Math.floor( boardColumns / 2 ),
-	    row: boardRows,
-	    phase: 0.0,
-	    d1: {x: 0, y: -1},
-	    d2: {x: 0, y: -1}};
-}
-
-
-function diagonalBounce( direction, ascending ) {
-    var m = ascending ? -1 : 1;
-    return { x: m * direction.y,
-	     y: m * direction.x };
-}
-
-function nextState( lastState ) {
-    var dir = lastState.d2;
-    var colp = lastState.col + dir.x;
-    var rowp = lastState.row + dir.y;
-    var element = elementAt( colp, rowp );
-    var phasep = lastState.phase - 1.0;
-    
-    if( !element || element.deactivated ) {
-	return { col: colp,
-		 row: rowp,
-		 phase: phasep,
-		 d1: dir,
-		 d2: dir };
-    }
-    if( element.type == "flipper" ) {
-	return { col: colp,
-		 row: rowp,
-		 phase: phasep,
-		 d1: dir,
-		 d2: diagonalBounce( dir, element.ascending ),
-		 resolve: function() {
-		     element.ascending = !element.ascending;
-		 }
-	       };
-    }
-    if( element.type == "square" || element.type == "breakable-square" ) {
-	if( element.type == "breakable-square" ) {
-	    removeElement( element );
-	}
-	return nextState( { col: colp,
-			    row: rowp,
-			    phase: phasep + 1.0,
-			    d2: {x: -dir.x, y: -dir.y } } );
-    }
-    if( element.type == "triangle" || element.type == "breakable-triangle" ) {
-	var tangent = [ [-1,1], [1,1], [1,-1], [-1,-1] ][ element.rotation ];
-	var ascending = element.rotation % 2;
-	var diagonal = (dir.x == -tangent[0]) || (dir.y == -tangent[1]);
-	var d2 = diagonal ? diagonalBounce( dir, ascending ) : {x: -dir.x, y: -dir.y};
-	if( diagonal ) {
-	    return { col: colp,
-		     row: rowp,
-		     phase: phasep,
-		     resolve: function() {
-			 if( element.type == "breakable-triangle" ) {
-			     removeElement( element );
-			 }
-		     },
-		     d1: dir,
-		     d2: diagonalBounce( dir, ascending ) };
-	} else {
-	    if( element.type == "breakable-triangle" ) {
-		removeElement( element );
-	    }
-	    return nextState( { col: colp,
-				row: rowp,
-				phase: phasep + 1.0,
-				d2: {x: -dir.x, y: -dir.y } } );
-	}
-    }
-    if( element.type == "switch" ) {
-	onEachElement( function(el) {
-	    if( elementDeactivatable(el) ) {
-		el.deactivated = ! el.deactivated;
-	    }
-	} );
-	return { col: colp,
-		 row: rowp,
-		 phase: phasep,
-		 d1: dir,
-		 d2: dir };
-    }
-}
-
-function cellAtPosition( pos ) {
-    var x = (pos.x - boardOffset.x) / cellsize;
-    var y = (pos.y - boardOffset.y) / cellsize;
-    if( x < 0 || y < 0 || x >= boardColumns || y >= boardRows ) {
-	return null;
-    }
-    return { col: Math.floor( x ),
-	     row: Math.floor( y ) };
-}
-
-function printObject( o ) {
-    for(var k in o) {
-	console.log( "key " + k + ": " + o[k] );
-    }
-}
-
-function getClickPosition( e ) {
-    if( Modernizr.touch ) {
-	console.log( "translating touch " );
-	if( e.touches && e.touches.length > 0 ) {
-	    return {x: e.touches[0].pageX - jqcanvas.offset().left,
-		    y: e.touches[0].pageY - jqcanvas.offset().top};
-	}
-    } else {
-	return {
-	    x: e.pageX - jqcanvas.offset().left,
-	    y: e.pageY - jqcanvas.offset().top
-	};
-    }
-}
-
 function arrayRemoveElement( ar, element ) {
     var i = ar.indexOf( element );
     if( i > -1 ) {
@@ -761,35 +583,11 @@ function arrayRemoveElement( ar, element ) {
     }
 }
 
-function removeElement( elt ) {
-    arrayRemoveElement( elements, elt );
+function elementDeactivatable( element ) {
+    return element.type != "switch";
 }
 
-function elementDeactivatable( el ) {
-    return el.type != "switch";
-}
-
-function removeElementAt( cell ) {
-    if( !buildMode ) {
-	return;
-    }
-
-    var element = elementAt( cell.col, cell.row );
-    if( element ) {
-        removeElement( element );
-    }
-}
-
-function toggleElementAt( cell ) {
-    if( !buildMode ) {
-	return;
-    }
-
-    var element = elementAt( cell.col, cell.row );
-    if( element ) {
-	removeElement( element );
-    }
-
+function cycleElements( element ) {
     var newElement = null;
 
     if( element && !element.deactivated && elementDeactivatable( element ) ) {
@@ -816,19 +614,15 @@ function toggleElementAt( cell ) {
     }
 
     if( newElement ) {
-	elements.push( newElement );
+	return newElement;
     }
-}
-
-function toggleGame() {
-    if( buildMode ) {
-	startRun();
-    } else {
-	stopRun();
-    }
+    return {type: "flipper", col: element.col, row: element.row, ascending: false };
 }
 
 function initialize() {
+    var canvasWidth = 480;
+    var canvasHeight = 480;
+
     canvas = document.createElement( "canvas" );
     canvas.id = "flippersCanvas";
     canvas.width = canvasWidth;
@@ -836,10 +630,56 @@ function initialize() {
 
     jqcanvas = $(canvas);
 
-    myState = GameState.loadOld(
+    var myState = GameState.loadOld(
 	{"rows":7,"cols":7,"contents":[{"type":"flipper","col":4,"row":2,"ascending":false},{"type":"flipper","col":3,"row":1,"ascending":true},{"type":"flipper","col":5,"row":1,"ascending":false},{"type":"flipper","col":5,"row":3,"ascending":true},{"type":"flipper","col":5,"row":4,"ascending":true},{"type":"flipper","col":4,"row":4,"ascending":false},{"type":"flipper","col":2,"row":2,"ascending":true},{"type":"flipper","col":2,"row":3,"ascending":false},{"type":"flipper","col":1,"row":0,"ascending":false},{"type":"flipper","col":0,"row":0,"ascending":true},{"type":"flipper","col":0,"row":3,"ascending":false},{"type":"flipper","col":1,"row":3,"ascending":true}]}
     );
+    var mySmoothState = null;
+
     myState.start();
+
+    function render( gfx ) {
+	if( mySmoothState ) {
+	    mySmoothState.render( gfx );
+	} else {
+	    myState.render( gfx );
+	}
+    }
+
+    function setState( newstate ) {
+	myState = newstate;
+	mySmoothState = null;
+    }
+
+    function startGame() {
+	myState.start();
+	mySmoothState = SmoothGameState.wrap( myState );
+	mySmoothState.start();
+    }
+
+    function stopGame() {
+	myState.stop();
+	mySmoothState = null;
+    }
+
+    function running() {
+	return mySmoothState != null;
+    }
+
+    function toggleGame() {
+	if( running() ) {
+	    stopGame();
+	} else {
+	    startGame();
+	}
+    }
+
+    function saveLevel() {
+	return JSON.stringify( myState.save() );
+    }
+
+    function loadLevel( data ) {
+	setState( GameState.load( JSON.parse( data ) ) );
+    }
 
     console.log( "Game beginning!");
 
@@ -897,30 +737,30 @@ function initialize() {
 
 	    return {
 		hold: function( m ) {
-		    removeElementAt( cell );
+		    myState.removeElementAtCell( cell );
 		},
 		tap: function( m ) {
-		    toggleElementAt( cell );
+		    var element = cycleElement( myState.elementAtCell( cell ) );
+		    myState.setElement( element );
 		}
 	    }
 	}
     );
 
-    gamegraphics = DiagramGraphics.create( canvas,
-					   {x: 0,
-					    y: 0,
-					    width: 480,
-					    height: 480},
-					   {cols: 9,
-					    rows: 9}
-					   );
-    var smooth = SmoothGameState( myState );
+    vargamegraphics = DiagramGraphics.create( canvas,
+					      {x: 0,
+					       y: 0,
+					       width: 480,
+					       height: 480},
+					      {cols: 9,
+					       rows: 9}
+					    );
+    var smooth = SmoothGameState.wrap( myState );
     smooth.start();
 
     setInterval( function() {
 	ctx.clearRect( 0, 0, canvas.width, canvas.height );
-
-	smooth.render( gamegraphics );
+	render( gamegraphics );
     }, 1000.0 / 30.0 );
 }
 
@@ -942,32 +782,3 @@ function declareResult( result ) {
     }
 }
 
-function drawFrame() {
-    ctx.clearRect( 0, 0, canvas.width, canvas.height );
-    
-    drawChessboard( {cols: boardColumns, rows: boardRows} );
-    
-    onEachElement( drawThing );
-
-    if( gamestate ) {
-	var pos = ballPosition();
-	drawBall( pos.x, pos.y );
-    }
-}
-
-// Proxies temporary
-function drawThing( thing ) {
-    gamegraphics.drawElement( thing );
-}
-
-function drawBall( cx, cy ) {
-    gamegraphics.drawBall( {x: cx, y: cy} );
-}
-
-function drawChessboard() {
-    gamegraphics.drawBackground();
-}
-
-function autofitBoard() {
-    gamegraphics.setBoardSize( {cols: boardColumns, rows: boardRows } );
-}
