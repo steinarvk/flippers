b4914c577e655ea90d84931618805509e2a89ddc
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-10-07 22:01:54 +0200

    another test for the bugfix #93

--- test/Solver-test.js
+++ test/Solver-test.js
@@ -28,5 +28,11 @@ buster.testCase( "Solver scenarios", {
                                    } );
 	buster.assert.less( report.ticks, 10 );
         buster.assert.equals( report.position, {col: -1, row: 1} );
+    },
+    "infinite loop": function() {
+        var report = Solver.solve( {"size":{"cols":7,"rows":7},"origin":{"col":3,"row":7},"initialVelocity":{"dx":0,"dy":-1},"target":{"col":3,"row":7},"elements":[{"col":3,"row":4,"type":"square","colour":"red","deactivated":true},{"col":3,"row":2,"type":"square","colour":"red","deactivated":true},{"col":3,"row":3,"type":"switch","colour":"red"}],"ball":{"position":{"col":3,"row":7},"incomingVelocity":{"dx":0,"dy":1},"outgoingVelocity":{"dx":0,"dy":1}},"status":"gameover:win" } );
+        
+        buster.assert.greater( report.ticks, 100 );
+        buster.assert.equals( report.result, null );
     }
 } );
