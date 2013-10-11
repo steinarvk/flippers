67d4b551032d55cbfe756f796631a74dd6c6805d
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-10-11 13:05:28 +0200

    sketch of an actual naive brute-force puzzle solver

--- src/Solver.js
+++ src/Solver.js
@@ -49,8 +49,75 @@ var Solver = (function() {
 	return reportState( context, game );
     }
 
+    function search( puzzle, options ) {
+	// Puzzle has .inventory, and is otherwise a start state.
+	// We can expect that each puzzle expands to ~ 2 x 40^puzzle.inventory.length potential solutions
+	// This means we're capable of naive exhaustive search for up to around 3 puzzle pieces, meaning most puzzles.
+	// We can also cut down the search space by adjoining a piece at a time and always placing it
+	// in the path of the ball.
+	// (However, note that this means we would have to try all possible orders of placing the puzzle pieces.)
+
+	// Naive search
+	var baseState = GameState.load( puzzle );
+	var moveCells = [];
+	for(var i = 0; i < puzzle.size.cols; i++) {
+	    for( var j = 0; j < puzzle.size.rows; j++) {
+		var cell = {col: i, row: j};
+		if( !baseState.elementAt( cell ) ) {
+		    moveCells.push( cell );
+		}
+	    }
+	}
+
+	var sets = [];
+	var solutions = [];
+
+	for(var i = 0; i < puzzle.inventory.length; i++) {
+	    sets.push( moveCells );
+	    sets.push( pieceConfigurations( puzzle.inventory[i] ) );
+	}
+
+	var gen = Generator.filter(
+	    Generator.cartesianProduct( sets ),
+	    function ( moves ) {
+		// Check that all the locations are unique
+		for(var i = 0; i < moves.length; i += 2) {
+		    if( moves.indexOf( moves[i] ) < i ) {
+			return false;
+		    }
+		}
+		return true;
+	    }
+	);
+	
+	while(true) {
+	    var state = JSON.parse( JSON.stringify( puzzle ) );
+	    var n = gen();
+
+	    if( n === undefined ) {
+		break;
+	    }
+
+	    for(var i = 0; i < n.length; i += 2) {
+		var cell = n[i];
+		var piece = n[i+1];
+		state.setElement( $.extend( {},
+					    cell,
+					    piece ) );
+	    }
+
+	    var result = Solver.solve( state );
+	    if( result.result == "win" ) {
+		solutions.push( JSON.stringify( state ) );
+	    }
+	}
+
+	return Util.uniqueElements( solutions ).map( JSON.parse );
+    }
+
     return {
-	solve: solve
+	solve: solve,
+	search: search
     };
 })();
 
