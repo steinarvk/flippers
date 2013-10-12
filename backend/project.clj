d78f156831d647892c6da412b42cdbe8ac2c7a65
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-10-12 12:28:34 +0200

    handling parameters on the backend

--- backend/project.clj
+++ backend/project.clj
@@ -1,7 +1,11 @@
 (defproject backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Flippers backend"
+  :url "http://irrasjonal.net/flippers-server/"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.ashafa/clutch "0.3.1"]
+                 [cheshire "5.2.0"]
+                 [ring-json-params "0.1.3"]
+                 [hiccup "1.0.4"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler backend.handler/app}
--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -1,12 +1,32 @@
 (ns backend.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
+            [com.ashafa.clutch :as clutch]
+            [cheshire.core :as json]
+            [hiccup.core :as hiccup]
+            [ring.middleware.params :as params]
             [compojure.route :as route]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(def main-page
+  (hiccup/html
+   [:form {:action "/flippers-server/add-puzzle"
+           :method "POST"}
+    [:textarea {:name "puzzledata"}]
+    [:input {:type "submit"}]]))
+
+(defn feedback [message]
+  (hiccup/html
+   [:p message]
+   [:a {:href "/flippers-server/"} "Back"]))
+
+(defroutes app-core-routes
+  (POST "/flippers-server/add-puzzle" [puzzledata]
+        (feedback (str puzzledata)))
+  (GET "/flippers-server/" [] main-page)
   (route/resources "/")
   (route/not-found "Not Found"))
 
+(def app-routes (params/wrap-params app-core-routes))
+             
 (def app
   (handler/site app-routes))
