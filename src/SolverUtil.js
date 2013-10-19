efee794ff570be371c7d4793d1cac29decdca300
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-10-19 17:49:30 +0200

    better solver

---
+++ src/SolverUtil.js
@@ -0,0 +1,137 @@
+"use strict";
+
+var fs = require("fs");
+
+var Solver = require("./Solver");
+var Util = require("./Util");
+var Map2D = require("./Map2D" );
+
+var SolverUtil = (function() {
+    function analyzePuzzle( puzzle ) {
+        var solutions = Solver.search( puzzle );
+        return solutions.map( function( solution ) {
+            return {solution: solution,
+                    analysis: analyzeSolution( solution )};
+        } );
+    }
+
+    function analyzeSolution( solution ) {
+        var bounces = 0,
+            rollovers = 0,
+            switches = 0,
+            areaCovered = [],
+            stretches = [],
+            movedSinceInteraction = 0,
+            stretchCounts = null,
+            untouchedPieces = Map2D.create(),
+            touchedPieces = Map2D.create(),
+            i,
+            result;
+
+        for( i = 0; i < solution.elements.length; i++ ) {
+            untouchedPieces.set( solution.elements[i].col,
+                                 solution.elements[i].row,
+                                 true );
+        }
+
+        function flushInteraction() {
+            stretches.push( movedSinceInteraction );
+            movedSinceInteraction = 0;
+        }
+        
+        result = Solver.solve( solution, { hooks: {
+            onBallMoved: function( newCell, velocity ) {
+                areaCovered.push( [ newCell.col, newCell.row ].toString() );
+                ++movedSinceInteraction;
+            },
+            onInteraction: function( element, ballVel ) {
+                // element is before
+                areaCovered.push( [ element.col, element.row ].toString() );
+                flushInteraction();
+                if( element.type == "switch" ) {
+                    ++switches;
+                } else {
+                    ++bounces;
+                }
+                untouchedPieces.remove( element.col, element.row );
+                touchedPieces.adjust( element.col, element.row, function(x) {
+                    if( x ) {
+                        return x + 1;
+                    }
+                    return 1;
+                } );
+            },
+            onRollover: function( element, velocity ) {
+                areaCovered.push( [ element.col, element.row ].toString() );
+                ++rollovers;
+            }
+        } } );
+        
+        areaCovered = Util.uniqueElements( areaCovered ).length;
+        
+        stretchCounts = Util.arrayCounts( stretches );
+
+        return {
+            noninteractiveConnections: stretchCounts["0"] || 0,
+            bounces: bounces,
+            ticks: result.ticks,
+            switches: switches,
+            rollovers: rollovers,
+            squaresVisited: areaCovered,
+            untouchedPieces: untouchedPieces.count(),
+            multitouchedPieces: touchedPieces.save().map( Util.getter(2) ).filter( Util.greaterThan( 1 ) ).length
+        };
+    }
+
+    function summarizeSolution( report, output ) {
+        output( "Total elements",
+                report.solution.elements.length );
+        output( "Ticks taken",
+                report.analysis.ticks );
+        output( "Switches",
+                report.analysis.switches );
+        output( "Rollovers",
+                report.analysis.rollovers );
+        output( "Squares visited",
+                report.analysis.squaresVisited );
+        output( "Noninteractive connections",
+                report.analysis.noninteractiveConnections );
+        output( "Complexity (bounces)",
+                report.analysis.bounces );
+        output( "Never-hit pieces",
+                report.analysis.untouchedPieces );
+        output( "Multi-hit pieces",
+                report.analysis.multitouchedPieces );
+    }
+
+    function main( filenames ) {
+        var i, j, t0, t1, dt, filename, solutions, data;
+        for(i = 0; i < filenames.length; i++) {
+            filename = filenames[i];
+            console.log( "=== " + filename + "===" );
+            data = JSON.parse( fs.readFileSync( filename, "utf8" ) );
+            t0 = new Date().getTime();
+            solutions = SolverUtil.analyzePuzzle( data );
+            t1 = new Date().getTime();
+            dt = t1 - t0;
+            console.log( " " + solutions.length
+                         + " solution(s) found in "
+                         + dt + "ms" );
+            for(j = 0; j < solutions.length; j++) {
+                console.log( " == Solution " + (j+1) + " ==" );
+                summarizeSolution( solutions[j], function(name, value) {
+                    console.log( "  " + name + ": " + value );
+                } );
+            }
+        }
+    }
+
+    return {
+        main: main,
+        summarizeSolution: summarizeSolution,
+        analyzePuzzle: analyzePuzzle,
+        analyzeSolution: analyzeSolution
+    };
+}());
+
+module.exports = SolverUtil;
--- test/Solver-test.js
+++ test/Solver-test.js
@@ -1,9 +1,59 @@
 var buster = require("buster");
 
 var Solver = require("../src/Solver");
+var SolverUtil = require("../src/SolverUtil");
 
 var GameState = require("../src/GameState");
 
+var TestPuzzles = {
+    one:
+    {"size": {"cols":7,"rows":7},
+     "origin":{"col":3,"row":7},
+     "initialVelocity":{"dx":0,"dy":-1},
+     "target":{"col":3,"row":-1},
+     "inventory": [{type: "breakable-square"},
+                   {type: "breakable-triangle"}],
+     "elements":[{"col":3,"row":6,"type":"breakable-triangle","rotation":1},
+                 {"col":6,"row":6,"type":"breakable-triangle","rotation":3},
+                 {"col":6,"row":3,"type":"breakable-triangle","rotation":0},
+                 {"col":1,"row":3,"type":"breakable-triangle","rotation":1},
+                 {"col":0,"row":1,"type":"breakable-triangle","rotation":1},
+                 {"col":4,"row":1,"type":"breakable-triangle","rotation":0},
+                 {"col":4,"row":4,"type":"breakable-square"},
+                 {"col":3,"row":5,"type":"breakable-triangle","rotation":3},
+                 {"col":3,"row":0,"type":"breakable-triangle","rotation":1},
+                 {"col":1,"row":0,"type":"breakable-triangle","rotation":1},
+                 {"col":0,"row":4,"type":"breakable-triangle","rotation":2},
+                 {"col":0,"row":5,"type":"breakable-triangle","rotation":1},
+                 {"col":1,"row":6,"type":"breakable-triangle","rotation":3},
+                 {"col":0,"row":6,"type":"breakable-triangle","rotation":2},
+                 {"col":5,"row":2,"type":"breakable-triangle","rotation":3},
+                 {"col":1,"row":1,"type":"breakable-triangle","rotation":1},
+                 {"col":4,"row":0,"type":"breakable-triangle","rotation":0},
+                 {"col":5,"row":1,"type":"breakable-triangle","rotation":0},
+                 {"col":2,"row":2,"type":"breakable-square"},
+                 {"col":6,"row":2,"type":"breakable-triangle","rotation":3},
+                 {"col":6,"row":0,"type":"breakable-triangle","rotation":0}]
+     },
+    two:
+    {"size":{"cols":3,"rows":3},
+     "origin":{"col":1,"row":3},
+     "initialVelocity":{"dx":0,"dy":-1},
+     "target":{"col":1,"row":-1},
+     "elements":[{"col":1,"row":1,
+                  "type":"flipper",
+                  "ascending":true,"colour":"red"},
+                 {"col":2,"row":1,
+                  "type":"flipper",
+                  "ascending":true,"colour":"red"},
+                 {"col":2,"row":0,
+                  "type":"flipper",
+                  "ascending":false,"colour":"red"}],
+     "inventory": [{type: "flipper"}]
+    }
+
+};
+  
 buster.testCase( "Solver scenarios", {
     "basic ascending flip": function() {
         var report = Solver.solve( {size: {cols: 3, rows: 3},
@@ -41,21 +91,7 @@ buster.testCase( "Solver scenarios", {
 
 buster.testCase( "Solver search", {
     "simple": function() {
-        var puzzle = {"size":{"cols":3,"rows":3},
-                      "origin":{"col":1,"row":3},
-                      "initialVelocity":{"dx":0,"dy":-1},
-                      "target":{"col":1,"row":-1},
-                      "elements":[{"col":1,"row":1,
-                                   "type":"flipper",
-                                   "ascending":true,"colour":"red"},
-                                  {"col":2,"row":1,
-                                   "type":"flipper",
-                                   "ascending":true,"colour":"red"},
-                                  {"col":2,"row":0,
-                                   "type":"flipper",
-                                   "ascending":false,"colour":"red"}],
-                      "inventory": [{type: "flipper"}] };
-        var solutions = Solver.search( puzzle );
+        var solutions = Solver.search( TestPuzzles.two );
         buster.assert.equals( solutions.length, 1 );
 
         var solution = GameState.load( solutions[0] );
@@ -65,35 +101,24 @@ buster.testCase( "Solver search", {
         buster.assert.equals( placed.ascending, false );
     },
     "realistic uniqueness": function() {
-        var puzzle = {"size":{"cols":7,"rows":7},
-                      "origin":{"col":3,"row":7},
-                      "initialVelocity":{"dx":0,"dy":-1},
-                      "target":{"col":3,"row":-1},
-                      "inventory": [{type: "breakable-square"},
-                                    {type: "breakable-triangle"}],
-                      "elements":[{"col":3,"row":6,"type":"breakable-triangle","rotation":1},
-                                  {"col":6,"row":6,"type":"breakable-triangle","rotation":3},
-                                  {"col":6,"row":3,"type":"breakable-triangle","rotation":0},
-                                  {"col":1,"row":3,"type":"breakable-triangle","rotation":1},
-                                  {"col":0,"row":1,"type":"breakable-triangle","rotation":1},
-                                  {"col":4,"row":1,"type":"breakable-triangle","rotation":0},
-                                  {"col":4,"row":4,"type":"breakable-square"},
-                                  {"col":3,"row":5,"type":"breakable-triangle","rotation":3},
-                                  {"col":3,"row":0,"type":"breakable-triangle","rotation":1},
-                                  {"col":1,"row":0,"type":"breakable-triangle","rotation":1},
-                                  {"col":0,"row":4,"type":"breakable-triangle","rotation":2},
-                                  {"col":0,"row":5,"type":"breakable-triangle","rotation":1},
-                                  {"col":1,"row":6,"type":"breakable-triangle","rotation":3},
-                                  {"col":0,"row":6,"type":"breakable-triangle","rotation":2},
-                                  {"col":5,"row":2,"type":"breakable-triangle","rotation":3},
-                                  {"col":1,"row":1,"type":"breakable-triangle","rotation":1},
-                                  {"col":4,"row":0,"type":"breakable-triangle","rotation":0},
-                                  {"col":5,"row":1,"type":"breakable-triangle","rotation":0},
-                                  {"col":2,"row":2,"type":"breakable-square"},
-                                  {"col":6,"row":2,"type":"breakable-triangle","rotation":3},
-                                  {"col":6,"row":0,"type":"breakable-triangle","rotation":0}]};
-        var solutions = Solver.search( puzzle );
+        var solutions = Solver.search( TestPuzzles.one );
         buster.assert.equals( solutions.length, 1 );
+    }
+} );
+
+buster.testCase( "SolverUtil", {
+    "analysis": function() {
+        var reports = SolverUtil.analyzePuzzle( TestPuzzles.one ),
+            analysis = reports[0].analysis;
+        buster.assert.equals( reports.length, 1 );
+        buster.assert.isNumber( analysis.ticks );
+        buster.assert.isNumber( analysis.switches );
+        buster.assert.isNumber( analysis.rollovers );
+        buster.assert.isNumber( analysis.squaresVisited );
+        buster.assert.isNumber( analysis.noninteractiveConnections );
+        buster.assert.isNumber( analysis.bounces );
+        buster.assert.isNumber( analysis.untouchedPieces );
+        buster.assert.isNumber( analysis.multitouchedPieces );
     }
 } );
 
